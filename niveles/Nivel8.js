d7831c4dd208eee2cfe2ec2dbfdfe830a9596af2
Stefani Castellanos <scct95@hotmail.com>
2016-08-01 19:55:59 -0400

    Mejorando el nivel 8

--- niveles/Nivel8.js
+++ niveles/Nivel8.js
@@ -30,35 +30,36 @@ positivo hasta abajo.
     CrearPiso();
 
     // Puerta falsa
-    CrearSalida(570,486);
-    lineaizq = game.add.sprite(52, 470, 'linea');
-    lineaizq.scale.setTo(20, 1);
-    lineader = game.add.sprite(370, 470, 'linea');
-    lineader.scale.setTo(20, 1);
+    salidaFalsa = game.add.sprite(20,486, 'salida');
+    salidaFalsa.tint = 0xff9999;
+    lineaizq = game.add.sprite(20, 470, 'linea');
+    lineaizq.scale.setTo(5, 1);
+    lineader = game.add.sprite(150, 470, 'linea');
+    lineader.scale.setTo(5, 1);
 
     CrearSalida(700,486);
     CrearTimer();
     CrearPlay();
     limiteDeTiempo = 3;
-    lineaizq = game.add.sprite(52, 430, 'linea');
-    lineaizq.scale.setTo(30, 1);
-    lineader = game.add.sprite(400, 430, 'linea');
-    lineader.scale.setTo(30, 1);
+    lineaizq = game.add.sprite(230, 430, 'linea');
+    lineaizq.scale.setTo(20, 1);
+    lineader = game.add.sprite(500, 430, 'linea');
+    lineader.scale.setTo(20, 1);
     vector = CrearVector(400,300,0,0);
     cuadro = CrearCuadroVector(550,100,vector);
-    posInicXPlayer = 35;
+    posInicXPlayer = 200;
     posInicYPlayer = game.world.height - 110;
     CrearJugador(posInicXPlayer, posInicYPlayer);
     ecuacionVelocidad = CrearEcuacionVelocidad();
-    CrearDato(9,370,380,9,"distancia");
+    CrearDato(6,455,380,6,"distancia");
     CrearDato(3,700,510,3,"tiempo");
     tiempoFantasma = AñadirTexto(700,535,3,colorTiempo,48);
     tiempoFantasma.alpha = 0.7;
 
     //Datos falsos
-    CrearDato(8,290,420,8,"distancia");
-    CrearDato(2,570,510,2,"tiempo");
-    tiempoFantasma = AñadirTexto(570,535,2,colorTiempo,48);
+    CrearDato(3,100,430,3,"distancia");
+    CrearDato(1,20,510,1,"tiempo");
+    tiempoFantasma = AñadirTexto(20,535,1,colorTiempo,48);
     tiempoFantasma.alpha = 0.7;
 
 },
