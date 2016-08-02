b9ae19ab12d48d4b33e88f6b0d7bb06051977e6f
Sergio <alenios@hormail.com>
2016-08-01 22:06:10 -0400

    Se arregla el nivel 8

--- niveles/Nivel8.js
+++ niveles/Nivel8.js
@@ -26,17 +26,19 @@ positivo hasta abajo.
     CrearBasico();
 
     // Puerta falsa
-    CrearSalida(570,486);
-    lineaizq = game.add.sprite(52, 470, 'linea');
+    CrearSalida(700,486);
+    lineaizq = game.add.sprite(52, 450, 'linea');
     lineaizq.scale.setTo(20, 1);
-    lineader = game.add.sprite(370, 470, 'linea');
+    lineader = game.add.sprite(370, 450, 'linea');
     lineader.scale.setTo(20, 1);
 
-    CrearSalida(700,486);
+    CrearEspinas(630,440);
+
+    CrearSalida(570,486);
     limiteDeTiempo = 3;
-    lineaizq = game.add.sprite(52, 430, 'linea');
+    lineaizq = game.add.sprite(52, 380, 'linea');
     lineaizq.scale.setTo(30, 1);
-    lineader = game.add.sprite(400, 430, 'linea');
+    lineader = game.add.sprite(400, 380, 'linea');
     lineader.scale.setTo(30, 1);
     vector = CrearVector(400,300,0,0);
     cuadro = CrearCuadroVector(550,100,vector);
@@ -44,13 +46,13 @@ positivo hasta abajo.
     posInicYPlayer = game.world.height - 110;
     CrearJugador(posInicXPlayer, posInicYPlayer);
     ecuacionVelocidad = CrearEcuacionVelocidad();
-    CrearDato(9,370,380,9,"distancia");
+    CrearDato(9,370,330,9,"distancia");
     CrearDato(3,700,510,3,"tiempo");
     tiempoFantasma = AñadirTexto(700,535,3,colorTiempo,48);
     tiempoFantasma.alpha = 0.7;
 
     //Datos falsos
-    CrearDato(8,290,420,8,"distancia");
+    CrearDato(5,300,400,5,"distancia");
     CrearDato(2,570,510,2,"tiempo");
     tiempoFantasma = AñadirTexto(570,535,2,colorTiempo,48);
     tiempoFantasma.alpha = 0.7;
