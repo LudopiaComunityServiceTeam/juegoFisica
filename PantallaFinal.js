f29bba5e48cbe286fdc8362144796e8ac3e43d26
Virginia <vgil_22@hotmail.com>
2017-01-19 19:16:32 -0300

    implementada una pantalla de fin de juego temporal

---
+++ PantallaFinal.js
@@ -0,0 +1,32 @@
+var WebFontConfig = {
+
+    google: {
+        // Agregar la fuente de google que se quiere usar...
+        families: ['Indie Flower', 'Permanent Marker', 'Stardos Stencil']
+    }
+
+};
+
+var PantallaFinal = {
+preload: function() {
+
+},
+create: function() {
+    CrearFondo();
+    AñadirTexto(150,100,"¡Felicidades! ¡Acabaste el juego!",colorTexto,40);
+    AñadirTexto(300,150,"Tu puntuación es:",colorTexto,30);
+    ponerPuntuacionEnCuadro(350, 250);
+    AñadirTexto(300,400,"¿Quieres jugar de nuevo?",colorTexto,30);
+    botonMenu = game.add.sprite(300, 450, 'botonRepetirNivel');
+    botonMenu.frame = 0;
+    botonMenu.i = 0;
+    botonMenu.inputEnabled = true;
+    botonMenu.events.onInputDown.add(SeleccionarNivel, this);
+    botonMenu.events.onInputOver.add(overButton, this);
+    botonMenu.events.onInputOut.add(outButton, this);
+
+},
+update: function() {
+
+}
+};
