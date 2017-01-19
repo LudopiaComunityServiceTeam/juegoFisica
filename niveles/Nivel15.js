78651751d6cfb85c55c31574f859e44c11d221fa
Stefani Castellanos <scct95@hotmail.com>
2017-01-19 10:45:57 -0400

    El nivel 18 ahora es el 14 y los demas se corren. Utiliza la division

--- niveles/Nivel15.js
+++ niveles/Nivel15.js
@@ -23,126 +23,102 @@ positivo hasta abajo.
 */
 /********************************************************************************/
     CrearBasico();
+    botonPistas = CrearBotonPista("Un segundo, esas espinas \nse ven sospechosas,\nlo mejor sera no esperar mucho.");
     game.time.events.add(Phaser.Timer.SECOND * 10, ResaltarDudas, this);
-    botonPistas = CrearBotonPista("¡Pánico, números, espinas, ayuda!\n¿Donde esta el resolvedor? \nEspera...\nquiza los colores sean la clave.");
-    lineaizq = game.add.sprite(170, 475, 'linea');
-    lineaizq.scale.setTo(7, 1);
-    lineader = game.add.sprite(300, 475, 'linea');
-    lineader.scale.setTo(7, 1);
-    lineaizq2 = game.add.sprite(430, 475, 'linea');
-    lineaizq2.scale.setTo(7, 1);
-    lineader2 = game.add.sprite(550, 475, 'linea');
-    lineader2.scale.setTo(7, 1);
-    var posicionPuertaRealX = 512;
-    var posicionPuertaRealY = 384;
-    CrearSalida(posicionPuertaRealX,posicionPuertaRealY);
+    CrearSalida(720,262);
+    salidaAbierta = false;
+    CrearPlataforma(50,320,1,1);
+    CrearPlataforma(150,320,1,1);
+    CrearPlataforma(250,320,1,1);
+    CrearPlataforma(350,320,1,1);
+    CrearPlataforma(450,320,1,1);
+    CrearPlataforma(550,320,1,1);
+    CrearPlataforma(650,320,1,1);
+    limiteDeTiempo = Infinity;
+    CrearEspinas(200,180);
+    listaDeEspinas[0][0].anchor.setTo(0.5,0.5);
+    listaDeEspinas[0][0].angle = 270;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[0],[0,0],[2,-2],[500,500],[290,150],1));
+    CrearPropulsores(250,105,1);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[0],[0,0],[2,-2],[500,500],[215,75],1));
 
 
-    limiteDeTiempo = 1;
-    // Puerta falsa
-    var posicionPuertaFalsa1X = 612;
-    var posicionPuertaFalsa1Y = 484;
-    salidaFalsa1 = game.add.sprite(posicionPuertaFalsa1X,posicionPuertaFalsa1Y, 'salida');
-    // Puerta falsa
-    var posicionPuertaFalsa2X = 262;
-    var posicionPuertaFalsa2Y = 384;
-    salidaFalsa2 = game.add.sprite(posicionPuertaFalsa2X,posicionPuertaFalsa2Y, 'salida');
-    // Puerta falsa
-    var posicionPuertaFalsa3X = 162;
-    var posicionPuertaFalsa3Y = 484;
-    salidaFalsa3 = game.add.sprite(posicionPuertaFalsa3X,posicionPuertaFalsa3Y, 'salida');
+    CrearEspinas(200,450);
+    listaDeEspinas[1][0].anchor.setTo(0.5,0.5);
+    listaDeEspinas[1][0].angle = 90;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[1],[0,0],[-2,2],[500,500],[340,480],1));
+    CrearPropulsores(150,525);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[1],[0,0],[-2,2],[500,500],[415,555],1));
+
+    CrearEspinas(300,200);
+    listaDeEspinas[2][0].anchor.setTo(0.5,0.5);
+    listaDeEspinas[2][0].angle = 270;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[2],[0,0],[2,-2],[500,500],[290,150],1));
+    CrearPropulsores(350,125,1);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[2],[0,0],[2,-2],[500,500],[215,75],1));
+
+    CrearEspinas(300,430);
+    listaDeEspinas[3][0].anchor.setTo(0.5,0.5);
+    listaDeEspinas[3][0].angle = 90;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[3],[0,0],[-2,2],[500,500],[340,480],1));
+    CrearPropulsores(250,505);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[3],[0,0],[-2,2],[500,500],[415,555],1));
 
 
-//pared izq
-
-    CrearEspinas(150,240);
-    CrearEspinas(150,340);
-    CrearEspinas(150,440);
-    CrearEspinas(150,540);
-
-    listaDeEspinas[0][0].angle = 180;
-    listaDeEspinas[1][0].angle = 180;
-    listaDeEspinas[2][0].angle = 180;
-    listaDeEspinas[3][0].angle = 180;
-
-    pared = CrearPared(50,140);
-    pared = CrearPared(50,240);
-    pared = CrearPared(50,340);
-    pared = CrearPared(50,440);
-
-//techo
-
-    CrearEspinas(150,190);
-    CrearEspinas(250,190);
-    CrearEspinas(350,190);
-    CrearEspinas(450,190);
-    CrearEspinas(550,190);
-
+    CrearEspinas(400,220);
+    listaDeEspinas[4][0].anchor.setTo(0.5,0.5);
     listaDeEspinas[4][0].angle = 270;
-    listaDeEspinas[5][0].angle = 270;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[4],[0,0],[2,-2],[500,500],[290,150],1));
+    CrearPropulsores(450,145,1);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[4],[0,0],[2,-2],[500,500],[215,75],1));
+
+    CrearEspinas(400,410);
+    listaDeEspinas[5][0].anchor.setTo(0.5,0.5);
+    listaDeEspinas[5][0].angle = 90;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[5],[0,0],[-2,2],[800,800],[340,480],1));
+    CrearPropulsores(350,485);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[5],[0,0],[-2,2],[800,800],[415,555],1));
+
+    CrearEspinas(500,240);
+    listaDeEspinas[6][0].anchor.setTo(0.5,0.5);
     listaDeEspinas[6][0].angle = 270;
-    listaDeEspinas[7][0].angle = 270;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[6],[0,0],[2,-2],[800,800],[290,150],1));
+    CrearPropulsores(550,165,1);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[6],[0,0],[2,-2],[800,800],[215,75],1));
+
+    CrearEspinas(500,390);
+    listaDeEspinas[7][0].anchor.setTo(0.5,0.5);
+    listaDeEspinas[7][0].angle = 90;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[7],[0,0],[-2,2],[800,800],[340,480],1));
+    CrearPropulsores(450,465);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[7],[0,0],[-2,2],[800,800],[415,555],1));
+
+    CrearEspinas(600,260);
+    listaDeEspinas[8][0].anchor.setTo(0.5,0.5);
     listaDeEspinas[8][0].angle = 270;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[8],[0,0],[2,-2],[800,800],[290,150],1));
+    CrearPropulsores(650,185,1);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[8],[0,0],[2,-2],[800,800],[215,75],1));
 
-    techo = CrearPared(150,90);
-    techo.angle = 90;
+    CrearEspinas(600,370);
+    listaDeEspinas[9][0].anchor.setTo(0.5,0.5);
+    listaDeEspinas[9][0].angle = 90;
+    ListaDeCiclos.push(new Ciclo(listaDeEspinas[9],[0,0],[-2,2],[800,800],[340,480],1));
+    CrearPropulsores(550,445);
+    ListaDeCiclos.push(new Ciclo(listaDePropulsores[9],[0,0],[-2,2],[800,800],[415,555],1));
 
-    techo = CrearPared(250,90);
-    techo.angle = 90;
-
-    techo = CrearPared(350,90);
-    techo.angle = 90;
-
-    techo = CrearPared(450,90);
-    techo.angle = 90;
-
-    techo = CrearPared(550,90);
-    techo.angle = 90;
-
-    techo = CrearPared(650,90);
-    techo.angle = 90;
-
-    techo = CrearPared(750,90);
-    techo.angle = 90;
-
-//pared der
-
-    CrearEspinas(650,436);
-    CrearEspinas(650,336);
-    CrearEspinas(650,236);
-    CrearEspinas(650,136);
-
-    pared = CrearPared(700,436);
-    pared = CrearPared(700,336);
-    pared = CrearPared(700,236);
-    pared = CrearPared(700,136);
-
-    vector = CrearVector(630,200,0,0, true);
-
-    numeroAngulo = CrearAnguloParaVectorControlable(45,200,200,45);
-    numeroAngulo = CrearAnguloParaVectorControlable(180,350,200,180);
-    numeroAngulo = CrearAnguloParaVectorControlable(135,500,200,135);
-    numeroMagnitud = CrearNumeroParaVectorControlable(400,400,300,4);
-
-    CrearNube(posicionPuertaRealX-10,posicionPuertaRealY-50,posicionPuertaRealX+10,posicionPuertaRealY-10);
-    CrearNube(posicionPuertaFalsa1X-10,posicionPuertaFalsa1Y-50,posicionPuertaFalsa1X+10,posicionPuertaFalsa1Y-10);
-    CrearNube(posicionPuertaFalsa2X-10,posicionPuertaFalsa2Y-50,posicionPuertaFalsa2X+10,posicionPuertaFalsa2Y-10);
-    CrearNube(posicionPuertaFalsa3X-10,posicionPuertaFalsa3Y-50,posicionPuertaFalsa3X+10,posicionPuertaFalsa3Y-10);
-    CrearDato(1,posicionPuertaRealX,posicionPuertaRealY-76,1,"tiempo");
-    CrearDato(3,posicionPuertaFalsa1X,posicionPuertaFalsa1Y-76,3,"tiempo");
-    CrearDato(2,posicionPuertaFalsa2X,posicionPuertaFalsa2Y-76,2,"tiempo");
-    CrearDato(4,posicionPuertaFalsa3X,posicionPuertaFalsa3Y-76,4,"tiempo");
-    CrearDato(4,260,430,4,"distancia");
-    CrearDato(4,515,430,4,"distancia");
-    posInicXPlayer = 388;
-    posInicYPlayer = game.world.height - 110;
+    vector = CrearVector(300, 100, 300, 0, false);
+    vector2 = CrearVector(500, 100, 400, 0, false);
+    posInicXPlayer = 50;
+    posInicYPlayer = game.world.height - 350;
     //monologo de Sam
-    monologo = AñadirTexto(50,60,"A él, al problema... y a mi",colorTexto,20);
-    monologo.alpha = 0.01; 
+    monologo = AñadirTexto(500,300,"Lo vi desaparecer",colorTexto,20);
+    monologo.alpha = 0.01;
     faseBorrado = 0;
     //
-    CrearJugador(posInicXPlayer, posInicYPlayer);
-	DetenerCancion();
+    jugador = CrearJugador(posInicXPlayer, posInicYPlayer);
+    indice = 0;
+    DetenerCancion();
     ReproducirLoopAudio(Stop_And_Think,0.3);
 },
 
