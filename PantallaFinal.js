69a6e150e46e7dc1e047ddf7013d2943b73e0ede
Sergio <alenios@hormail.com>
2017-01-28 18:25:16 -0400

    Base de animación para el final

--- PantallaFinal.js
+++ PantallaFinal.js
@@ -6,15 +6,19 @@ var WebFontConfig = {
     }
 
 };
+var tiemposEscena = 0;
+var corriendo = false;
 
 var PantallaFinal = {
 preload: function() {
 
 },
 create: function() {
-    CrearFondo();
-    console.log(nivelActual);
-    console.log(Nota[nivelActual-2]);
+    CrearBasico()
+    posInicXPlayer = 75;
+    posInicYPlayer = game.world.height - 125;
+    CrearJugador(posInicXPlayer, posInicYPlayer);
+
     if (Nota[nivelActual-2] >= 10){
         AñadirTexto(150,50,"¡Felicidades! ¡Acabaste el juego\n y pasaste!",colorTexto,40);
         AñadirTexto(300,150,"Tu puntuación es:",colorTexto,30);
@@ -37,6 +41,40 @@ create: function() {
 
 },
 update: function() {
-
+    game.physics.arcade.collide(player, platforms);
+    
+    if ((tiemposEscena >= 0)&&(tiemposEscena < 400)){
+        MoverseALaDer(50);
+        //monologo de Sam
+        if (tiemposEscena == 50){
+            monologo = AñadirTexto(270,450,"Perrito!",colorTexto,20);
+            monologo.alpha = 0.01; 
+            faseBorrado = 0;
+        }
+        if (tiemposEscena > 50){
+            BorrarTexto();
+        }
+    }
+    if ((tiemposEscena >= 400)&&(tiemposEscena < 800)){
+        BorrarTexto();
+        if (tiemposEscena == 400){
+            Detenerse();
+        }
+    }
+    tiemposEscena = tiemposEscena + 1;
 }
 };
+function MoverseALaDer(velo) {
+    if (!corriendo)
+    {
+        player.animations.play('right');
+        corriendo = true;
+    }
+    player.body.velocity.x = velo;
+}
+function Detenerse() {
+    corriendo = false
+    player.body.velocity.x = 0;
+    player.animations.stop();
+    player.frame = 4;
+}
