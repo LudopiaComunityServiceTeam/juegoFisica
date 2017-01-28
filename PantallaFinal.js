fd6f4e689585e5f0fd83a7bafeb7514cbf62d9f5
Sergio <alenios@hormail.com>
2017-01-28 18:34:49 -0400

    Esquema del final

--- PantallaFinal.js
+++ PantallaFinal.js
@@ -14,30 +14,16 @@ preload: function() {
 
 },
 create: function() {
-    CrearBasico()
+    DetenerCancion();
+    ActivarFisica();
+    CrearFondo();
+    InicializarPlataformas();
+    CrearPiso();
     posInicXPlayer = 75;
     posInicYPlayer = game.world.height - 125;
     CrearJugador(posInicXPlayer, posInicYPlayer);
-
-    if (Nota[nivelActual-2] >= 10){
-        AñadirTexto(150,50,"¡Felicidades! ¡Acabaste el juego\n y pasaste!",colorTexto,40);
-        AñadirTexto(300,150,"Tu puntuación es:",colorTexto,30);
-        ponerPuntuacionFinal(350, 250);
-    }
-    else if (Nota[nivelActual-2] < 10){
-        AñadirTexto(150,100,"Acabaste el juego, pero no pasaste.",colorTexto,40);
-        AñadirTexto(300,150,"Tu puntuación es:",colorTexto,30);
-        ponerPuntuacionFinal(350, 250);
-    }
-
-    AñadirTexto(270,350,"¿Quieres jugar de nuevo?",colorTexto,30);
-    botonMenu = game.add.sprite(350, 400, 'botonRepetirNivel');
-    botonMenu.frame = 0;
-    botonMenu.i = 0;
-    botonMenu.inputEnabled = true;
-    botonMenu.events.onInputDown.add(SeleccionarNivel, this);
-    botonMenu.events.onInputOver.add(overButton, this);
-    botonMenu.events.onInputOut.add(outButton, this);
+    tiemposEscena = 0;
+    corriendo = false;
 
 },
 update: function() {
@@ -60,6 +46,26 @@ update: function() {
         if (tiemposEscena == 400){
             Detenerse();
         }
+    }
+    if (tiemposEscena == 800){
+        if (Nota[nivelActual-2] >= 10){
+            AñadirTexto(150,50,"¡Felicidades! ¡Acabaste el juego\n y pasaste!",colorTexto,40);
+            AñadirTexto(300,150,"Tu puntuación es:",colorTexto,30);
+            ponerPuntuacionFinal(350, 250);
+        }
+        else if (Nota[nivelActual-2] < 10){
+            AñadirTexto(150,100,"Acabaste el juego, pero no pasaste.",colorTexto,40);
+            AñadirTexto(300,150,"Tu puntuación es:",colorTexto,30);
+            ponerPuntuacionFinal(350, 250);
+        }
+        AñadirTexto(270,350,"¿Quieres jugar de nuevo?",colorTexto,30);
+        botonMenu = game.add.sprite(350, 400, 'botonRepetirNivel');
+        botonMenu.frame = 0;
+        botonMenu.i = 0;
+        botonMenu.inputEnabled = true;
+        botonMenu.events.onInputDown.add(SeleccionarNivel, this);
+        botonMenu.events.onInputOver.add(overButton, this);
+        botonMenu.events.onInputOut.add(outButton, this);
     }
     tiemposEscena = tiemposEscena + 1;
 }
